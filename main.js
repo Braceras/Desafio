3c50a6cae3a8ce3191f5747d69c0afafaabe5d46
Benja Braceras <benjabracerasm@gmail.com>
2022-06-09 04:18:52 -0300

    websocketApp

--- main.js
+++ main.js
@@ -1,55 +1,63 @@
 const express = require('express')
+const { Server: HttpServer } = require('http')
+const { Server: IOServer } = require('socket.io')
+
+
 const app = express()
+const httpServer = new HttpServer(app)
+const io = new IOServer(httpServer)
+
+
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.set('views', './views')
-//app.set('view engine', 'pug')
 app.set('view engine', 'ejs')
+app.use(express.static('./public'))
+
+let users = []
+
+app.get('/login', (req, res) => {
+    return res.render('login')
+})
 
 
-app.get('', (req, res) => res.json({ status: 'ok'}))
 
 
 const products = []
 
-app.get('/home', (req, res) => {
+app.get('', (req, res) => {
     const data = {
         message: 'Welcome!'
     }
-    return res.render('home', data)
+    return res.render('index', data)
 })
 
 app.get('/products', (req, res) => {
     const data = { 
-        title: 'List of Products',
         products
      }
     return res.render('products', data)
 })
 
-
-
 app.post('/products', (req, res) => {
     const product = {
         name: req.body.name,
         price: req.body.price,
-        img: req.body.img
+        img: req.body.img,
+        id:products.length+1
     }
 
     products.push(product)
-    
-
-    return res.render('products', product)
+    return res.send('products')
 })
+
 
 
 
 const PORT = 8080
 
-const server = app.listen(PORT, () => {
-    console.log(`Servidor HTTP corriendo en el puero ${PORT}`);
+httpServer.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
 })
-
-server.on('error', error => console.log(`Error en servidor: ${error}`))
